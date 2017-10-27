10b50ac66d5a568ff1fae0bcf0168a7d93c8ed67
greenfield932 <dtyugin@gmail.com>
2017-10-27 19:18:23 +0300

    add generators

--- model.py
+++ model.py
@@ -2,44 +2,76 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, MaxPooling2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers import Lambda
 from keras.layers import Cropping2D
 from keras.layers import Activation
 from keras import backend as K
 import matplotlib.pyplot as plt
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import sklearn
 
-lines = []
-fd = open('../data/record/driving_log.csv')
-reader = csv.reader(fd)
+def readInputData(driving_log_filename):
+    lines = []
+    fd = open(driving_log_filename)
+    reader = csv.reader(fd)
 
-cnt = 0
-for line in reader:
-    if cnt > 0:
-        lines.append(line)
-    cnt+=1
+    cnt = 0
+    for line in reader:
+        if cnt > 0:
+            lines.append(line)
+        cnt+=1
 
-center_img_idx = 0
-left_img_idx = 1
-right_img_idx = 2
-steering_idx = 3
-throttle_idx = 4
-break_idx = 5
-speed_idx = 6
+    center_img_idx = 0
+    left_img_idx = 1
+    right_img_idx = 2
+    steering_idx = 3
+    throttle_idx = 4
+    break_idx = 5
+    speed_idx = 6
 
-steering_data = []
-image_data = []
+    samples = []
 
-correction = 0.1 # this is a parameter to tune
+    correction = 0.25 # this is a parameter to tune
+    
+    for data in lines:
+        #image = cv2.imread(data[center_img_idx])
+        #image = Image.open(data[center_img_idx])
+        #image = np.asarray(image)
 
-for data in lines:
-    image = cv2.imread(data[center_img_idx])
-    image_data.append(image)
-    steering_center = float(data[steering_idx])
-    steering_data.append(steering_center)
-    if len(steering_data) > 5:
-        break
+        steering = float(data[steering_idx])
+        
+        samples.append([data[center_img_idx], steering])
+
+        samples.append([data[left_img_idx], steering + correction])
+
+        samples.append([data[right_img_idx], steering - correction])
+
+    return samples
+
+
+def drawImage(image):
+    f, ax = plt.subplots(1, 1, figsize=(5, 5))
+    ax.imshow(image)
+    ax.axis('off')
+    plt.show()
+
+def drawImages(images, rows, cols, labels = []):
+
+    fig = plt.figure(figsize = (cols*4,rows*2))
+#    plt.subplots_adjust(wspace = 0.3, hspace = 1)
+    
+    for i in range(0, len(images)):
+        image = images[i]
+        subplot = fig.add_subplot(rows, cols, i+1)
+        subplot.imshow(image)
+        if len(labels)>0:
+            subplot.set_title(labels[i])
+        subplot.axis('off')
+    plt.show()
+    
     #image_left = cv2.imread(data[left_img_idx])
     #steering_left = steering_center + correction
     #image_data.append(image_left)
@@ -50,34 +82,103 @@ for data in lines:
     #image_data.append(image_right)
     #steering_data.append(steering_right)
 
-X_train = np.array(image_data)
-y_train = np.array(steering_data)
+def evalLayer(model, image, idx):
+    cropping_output = K.function([model.layers[idx].input], [model.layers[idx].output])
+    return cropping_output([image[None,...]])[0]
+
+def augment(image, steering, augtype):
+    if augtype == 'f':        
+        image_flipped = np.fliplr(image)
+        steering_flipped = -steering
+        return image_flipped, steering_flipped
+    return image, steering
+        
+def generator(samples, batch_size=32):
+
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                image = Image.open(batch_sample[0])
+                image = np.asarray(image)
+                steering = batch_sample[1]
+                
+                images.append(image)
+                angles.append(steering)
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+samples = readInputData('../data/record/driving_log.csv')
+samples = np.array(samples)
+
+train, val = train_test_split(samples, test_size=0.2)
+
+train_generator = generator(train, batch_size=32)
+val_generator = generator(val, batch_size=32)
+
+n_train = len(train)
+n_val = len(val)
 
 model = Sequential()
-#model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
+#model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
+#model.add(MaxPooling2D(pool_size=(img_rows/2, img_cols/2)))
+#cropping_output = K.function([model.layers[0].input], [model.layers[0].output])
+#image = image_data[0]
+#cropped_image = cropping_output([image[None,...]])[0]
+#cropped_image = np.uint8(cropped_image[0,...])
+#drawImage(cropped_image)
 
-cropping_output = K.function([model.layers[0].input], [model.layers[0].output])
-image = image_data[0]
-cropped_image = cropping_output([image[None,...]])[0]
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Convolution2D(24, 5, 5, subsample=(1, 1), border_mode='same',  input_shape=(3,160,320)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
 
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-ax.imshow(cropped_image)
-plt.show()
+model.add(Convolution2D(48, 5, 5, subsample=(1, 1), border_mode='same',  input_shape=(3,160,320)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+
+model.add(Convolution2D(96, 3, 3, subsample=(2, 2), border_mode='same',  input_shape=(3,160,320)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Dense(64))
+model.add(Dense(1))
+
+#f, ax = plt.subplots(1, 1, figsize=(5, 5))
+#ax.imshow(cropped_image)
+#plt.show()
 #cropped_image = cropping_output([image[None,...]])[0]
 #model.add(Flatten(input_shape = (90, 320, 3)))
-#model.add(Convolution2D(24, 5, 5, border_mode='same', output_shape=(24,45,160)))
+
+#model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode='same',  input_shape=(3,160,320)))
 #model.add(Activation('relu'))
+
 #model.add(Convolution2D(36, 5, 5, border_mode='same'))
-#model.add(Activation('relu'))
 #model.add(MaxPooling2D(pool_size=(img_rows/2, img_cols/2)))
-#model.add(Flatten(input_shape = (90, 320, 3)))
+#model.add(Flatten())
 #model.add(Dense(1))
 
-#model.compile(loss = 'mse', optimizer = 'adam')
+model.compile(loss = 'mse', optimizer = 'adam')
+
 #model.summary()
-#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch = 15)
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch = 8)
 
-#model.save('model.h5')
+model.fit_generator(train_generator, samples_per_epoch=n_train,
+                    validation_data=val_generator, nb_val_samples=n_val,
+                    nb_epoch=3)
 
-
+model.save('model.h5')
