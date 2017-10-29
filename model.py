58b4ac3797ad14825dae1850029b4affab584f3e
greenfield932 <dtyugin@gmail.com>
2017-10-29 16:59:53 +0300

    update moidel.py

--- model.py
+++ model.py
@@ -7,14 +7,14 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers import Lambda
 from keras.layers import Cropping2D
 from keras.layers import Activation
-from keras import backend as K
+from keras import backend as K #for crop layer visualisation
 import matplotlib.pyplot as plt
 from PIL import Image
 from sklearn.model_selection import train_test_split
 import sklearn
-from keras.utils.visualize_util import plot 
-#from keras.utils import plot_model  
+from keras.utils.visualize_util import plot #for model graph visualization purposes
 
+#read driving log: images filenames and steering values
 def readInputData(driving_log_filename):
     lines = []
     fd = open(driving_log_filename)
@@ -40,25 +40,25 @@ def readInputData(driving_log_filename):
     
     for data in lines:
         steering = float(data[steering_idx])
-        
         samples.append([data[center_img_idx], steering])
-
         samples.append([data[left_img_idx], steering + correction])
-
         samples.append([data[right_img_idx], steering - correction])
         
     return samples
 
+# read image data from file
 def readImage(filename):
     image = Image.open(filename)
     return np.asarray(image)
 
+# draw image via matplot
 def drawImage(image):
     f, ax = plt.subplots(1, 1, figsize=(5, 5))
     ax.imshow(image)
     ax.axis('off')
     plt.show()
 
+# draw multiple images via matplot
 def drawImages(images, rows, cols, labels = []):
 
     fig = plt.figure(figsize = (cols*4,rows*2))   
@@ -71,10 +71,10 @@ def drawImages(images, rows, cols, labels = []):
         subplot.axis('off')
     plt.show()
 
+# augment data by flipping and brighthness
 def augment(image, steering, augtype):
     if augtype == 'flip':        
-        image_flipped = np.fliplr(image)
-               
+        image_flipped = np.fliplr(image)               
         steering_flipped = -steering
         return image_flipped, steering_flipped
     elif augtype == 'bright':
@@ -88,7 +88,10 @@ def augment(image, steering, augtype):
         return image, steering    
     return image, steering
 
+# augmentation multiplier - adjust samples size depending on augmentation techniques amount
 augment_multiplier = 2
+
+# data generator
 def generator(samples, batch_size=32*augment_multiplier):
 
     num_samples = len(samples)
@@ -117,6 +120,7 @@ def generator(samples, batch_size=32*augment_multiplier):
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
+# for report purposes - draw original and flipped image
 def showFlip(samples):
     idx = 99
     img0 = readImage(samples[idx][0])
@@ -124,7 +128,7 @@ def showFlip(samples):
     img1, steer_flip = augment(img0, steer, 'flip')
     drawImages([img0, img1], 1, 2, ["original: {0:.2f}".format(steer) , "flipped: : {0:.2f}".format(steer_flip)] )
 
-
+# for report purposes - draw left/center/right images and steering angles
 def showCorrection(samples):
     idx = 0
     img_center = readImage(samples[idx][0])
@@ -137,17 +141,19 @@ def showCorrection(samples):
 
     drawImages([img_left, img_center, img_right], 1, 3, ['left_image:'+steer_left, 'center_image:'+steer_center, 'right_image:'+steer_right])
 
+# for report purposes - draw cropped image using keras lambda layer
 def showCrop(model, samples):
-
     image = readImage(samples[0][0])
     cropping_output = K.function([model.layers[0].input], [model.layers[0].output])
     cropped_image = cropping_output([image[None,...]])[0]
     cropped_image = np.uint8(cropped_image[0,...])
     drawImages([image, cropped_image],1,2,['original','cropped'])
-    
+
+# for report purposes - draw model graph  
 def drawModel(model):
     plot(model, to_file='model.png',show_shapes=True, show_layer_names=True)  
 
+# draw train/val loss plot
 def drawLoss(history):
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -156,7 +162,7 @@ def drawLoss(history):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     plt.show()
-    
+
 samples = readInputData('../data/record/driving_log.csv')
 samples = np.array(samples)
 
@@ -168,31 +174,32 @@ val_generator = generator(val, batch_size=32)
 n_train = len(train)*augment_multiplier
 n_val = len(val)*augment_multiplier
 
+#CNN model definition
 model = Sequential()
 
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3))) #crop image to remove non relevant data (sky and car head)
 
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5)) # normalize data (mean = 0)
 
-model.add(Convolution2D(24, 5, 5, subsample=(1, 1), border_mode='valid'))
+model.add(Convolution2D(24, 5, 5, subsample=(1, 1), border_mode='valid')) #first convolutional layer 24 filters, pooling, dropout, ReLU activation
 model.add(MaxPooling2D((2, 2)))
 model.add(Dropout(0.4))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid'))  #second convolutional layer 48 filters, pooling, dropout, ReLU activation
 model.add(MaxPooling2D((2, 2)))
 model.add(Dropout(0.4))
 model.add(Activation('relu'))
 
-model.add(Convolution2D(96, 3, 3, subsample=(2, 2), border_mode='valid'))
+model.add(Convolution2D(96, 3, 3, subsample=(2, 2), border_mode='valid'))  #third convolutional layer 96 filters, pooling, dropout, ReLU activation
 model.add(MaxPooling2D((2, 2)))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Dense(64))
-model.add(Dense(1))
+model.add(Flatten()) # Flatten
+model.add(Dense(128)) #Fully connected 1
+model.add(Dense(64)) #Fully connected 2
+model.add(Dense(1)) #Fully connected 3
 
 model.compile(loss = 'mse', optimizer = 'adam')
 
